778c05511249e9a6966968a03abf996b8d10458b
Joao Alves <aluno19262@ipt.pt>
2019-04-02 17:00:32 +0100

    criaçao  inicial da base de dados

---
+++ Multas/Multas/Models/Multas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{
+    public class Multas
+    {
+        public int Id { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public Decimal ValorMulta { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
---
+++ Multas/Multas/Models/MultasDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{
+    public class MultasDB : DbContext
+    {
+        //definir as tabelas da BD
+
+        public DbSet< Condutores> Condutores { get; set; }
+        public DbSet<Agentes> Agentes { get; set; }
+        public DbSet<Multas> Multas { get; set; }
+        public DbSet<Viaturas> Viaturas { get; set; }
+
+    }
+
+}
