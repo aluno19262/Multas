45d89ee7a85b717ea8d162e25f623f95ff58b440
Joao Alves <aluno19262@ipt.pt>
2019-05-03 19:26:18 +0100

    adiçao de anotadores a classe dos agentes  para validar o tipo de dados introduzidos

--- Multas/Multas/Models/Agentes.cs
+++ Multas/Multas/Models/Agentes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,17 @@ namespace Multas.Models
 {
     public class Agentes
     {
+        [Key]
         public int ID { get; set; }
-
+        [Required (ErrorMessage ="O Nome é de preenchimento obrigatório.")]
+        [StringLength(40)]
+        [RegularExpression("", ErrorMessage ="só são aceites palavras começadas por letra Maiscula, separadas por um espaço em branco")]
         public string Nome { get; set; }
-
+        [Required (ErrorMessage = "A Esquadra é de preenchimento obrigatório.")]
+        [StringLength(30)]
         public string Esquadra { get; set; }
-
+        //[Required (ErrorMessage = "A Fotografia é de preenchimento obrigatório.")]
+        [StringLength(20)]
         public string Fotografia { get; set; }
 
         //lista das multas associadas ao agente
